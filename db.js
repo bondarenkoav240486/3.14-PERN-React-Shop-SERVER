27bfbaa6931341acc62329fddce26b7044a60e4a
Anton Bondarenko <bondarenko.av24@gmail.com>
2024-08-10 19:22:36 +0300

    add ch to db.js    SSL

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // ПАРОЛЬ
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+// module.exports = new Sequelize(
+//     process.env.DB_NAME, // Название БД
+//     process.env.DB_USER, // Пользователь
+//     process.env.DB_PASSWORD, // ПАРОЛЬ
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT
+//     }
+// )
 
 // const pool = new Pool({
 //     connectionString: process.env.POSTGRES_URL ,
@@ -23,3 +23,20 @@ module.exports = new Sequelize(
 //         dialect: 'postgres',
 //     }
 // )
+
+module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
